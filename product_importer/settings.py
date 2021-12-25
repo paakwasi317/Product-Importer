dfca80acb638e54938c8fc382c64ccc545a1df31
Paa Kwasi oppong <paakwasi317@gmail.com>
2021-12-25 21:01:56 +0000

    Add paginanation config to settings

--- product_importer/settings.py
+++ product_importer/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     },
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
