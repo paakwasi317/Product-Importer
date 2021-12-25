5db6e267611c9a7bf104851a4cb25a6f9740feab
Paa Kwasi oppong <paakwasi317@gmail.com>
2021-12-25 21:01:19 +0000

    Override serializer create method

--- workflow/serializers.py
+++ workflow/serializers.py
@@ -3,6 +3,18 @@ from workflow.models import Products
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    """
+        overriding create method to over-write duplicate products with same sku
+    """
+    def create(self, validated_data):
+        product = Products.objects.filter(sku=validated_data.get('sku')).first()
+        if not product:
+            instance = Products.objects.create(**validated_data)
+        else:
+            instance = super(ProductSerializer, self).update(product, validated_data)
+        return instance
+
     class Meta:
         model = Products
         fields = [
